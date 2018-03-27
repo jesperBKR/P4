97d4ea4555cded52f2899d566b4f3cfbb5c8a6ba
Jón Davíð <guni_29@hotmail.com>
2018-03-27 13:57:05 +0200

    Added Blob Analysis example

---
+++ Object and Blob/src/BlobAnalysis.cpp
@@ -0,0 +1,50 @@
+
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+  const char * my_window = "Apple Window, but not Apple inc.";
+
+  Mat appleImage = imread("data/apple.jpg");
+  Mat morp = getStructuringElement( MORPH_ELLIPSE , Size(19, 19), Point(-1, -1) );
+  Mat binImg = Mat(appleImage.rows, appleImage.cols, CV_8U);
+  Mat contourImg = Mat(appleImage.rows, appleImage.cols, CV_8UC3);
+
+
+
+  if(appleImage.empty())
+  {
+    std::cout <<"No image was loaded. Goodbye!" << std::endl;
+    return -1;
+  }
+  for(int x = 0; x < appleImage.cols; x++){
+    for(int y = 0; y < appleImage.rows; y++){
+      if(( appleImage.at<Vec3b>(y,x)[0] < 200
+        && appleImage.at<Vec3b>(y,x)[1] < 200
+        && appleImage.at<Vec3b>(y,x)[2] > 200 ) ||
+         ( appleImage.at<Vec3b>(y,x)[0] < 100
+        && appleImage.at<Vec3b>(y,x)[1] < 100
+        && appleImage.at<Vec3b>(y,x)[2] > 100 )) //appleImage.at<Vec3b>(y,x)[0] < 100 &&
+      {
+        binImg.at<char>(y, x) = 255;
+      } else {
+        binImg.at<char>(y, x) = 0;
+      }
+    }
+  }
+
+  morphologyEx(binImg, binImg, MORPH_CLOSE, morp, Point(-1, -1));
+  morphologyEx(binImg, binImg, MORPH_OPEN, morp, Point(-1, -1));
+  namedWindow(my_window);
+  imshow(my_window, binImg);
+
+  waitKey(0);
+
+  return 0;
+
+}
