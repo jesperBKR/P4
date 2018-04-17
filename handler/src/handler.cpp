3fcf65a4f5f07de9ef0f385ccef0999752786287
LarsGS <larsgs@live.dk>
2018-04-17 13:16:53 +0200

    Added message generation

--- handler/src/handler.cpp
+++ handler/src/handler.cpp
@@ -6,53 +6,85 @@
 #include <geometry_msgs/Point32.h>
 #include<std_msgs/Int32.h>
 #include<std_msgs/Bool.h>
-#include "../msg/Setup.h"
-#include "../msg/object_pos.h"
+#include "rupee_msgs/Setup.h"
+#include "rupee_msgs/object_pos.h"
 
 using namespace ros;
 
-//Input values from GUI: reps, diff, and type
-void guiCallback(const testing::Setup gui_msg){
-  ROS_INFO("I heard: [%d], [%d], [%d]", gui_msg.reps,gui_msg.diff,gui_msg.type);
-}
-//Object position, xyz
-void positionCallback(const geometry_msgs::Point32& position_msg){
-  ROS_INFO("I heard: [%f], [%f], [%f]", position_msg.x,position_msg.y,position_msg.z);
-}
-//JACO completed number of repetitions
-void processCallback(const std_msgs::Bool& process_msg){
-  ROS_INFO("I head: [%d]",process_msg.data);
+//Global Variables
+Rate loop_rate(10);
 
+//Class
+class Handler{
+public:
+  Handler(){
+    //Subscribers
+    Subscriber position_sub = nh.subscribe("object_pos", 1, &Handler::positionCallback,this);
+    Subscriber gui_sub = nh.subscribe("This_topic", 1, &Handler::guiCallback,this);
+    Subscriber process_sub = nh.subscribe("process_JACO",1,&Handler::processCallback,this);
 
-}
+    //Publishers
+    ros::Publisher move_pub = nh.advertise<rupee_msgs::object_pos>("move_to", 1);
+    ros::Publisher feedback_pub = nh.advertise<std_msgs::Int32>("feedback", 1);
+  }
+  ~Handler();
+
+  //Input values from GUI: reps, diff, and type
+  void guiCallback(const rupee_msgs::Setup gui_msg){
+    ROS_INFO("I heard: [%d], [%d], [%d]", gui_msg.reps,gui_msg.diff,gui_msg.type);
+    if(gui_msg.reps == 0){
+      start = false;
+    }
+    else{
+      start = true;
+    }
+
+  }
+  //Object position, xyz
+  void positionCallback(const geometry_msgs::Point32& position_msg){
+    ROS_INFO("I heard: [%f], [%f], [%f]", position_msg.x,position_msg.y,position_msg.z);
+  }
+  //JACO number of repetitions completed
+  void processCallback(const std_msgs::Bool& process_msg){
+    ROS_INFO("I head: [%d]",process_msg.data);
+    if(start){
+      moveit.move.data = true;
+      std::cout << "x-position: " << std::endl;
+      std::cin >> moveit.location.x;
+      std::cout << "y-position: " << std::endl;
+      std::cin >> moveit.location.y;
+      std::cout << "z-position: " << std::endl;
+      std::cin >> moveit.location.z;
+      move_pub.publish(moveit);
+    }
+  }
+
+private:
+  //ROS
+  NodeHandle nh;
+  Subscriber position_sub;
+  Subscriber gui_sub;
+  Subscriber process_sub;
+  Publisher move_pub;
+  Publisher feedback_pub;
+
+  //Variables and objects
+  rupee_msgs::object_pos moveit;
+  bool start;
+};
+
 
 int main(int argc, char ** argv){
 
   init(argc, argv, "handler_node");
-  NodeHandle nh;
-  ros::Publisher move_pub = nh.advertise<handler::object_pos>("move_to", 1);
-  //Subscribers
-  Subscriber position_sub = nh.subscribe("object_pos", 1, positionCallback);
-  Subscriber gui_sub = nh.subscribe("This_topic", 1, guiCallback);
-  Subscriber process_sub = nh.subscribe("process_JACO",1,processCallback);
+  Handler handler;
 
-  //Publishers
-  ros::Publisher feedback_pub = nh.advertise<std_msgs::Int32>("feedback", 1);
+  while (true||ok()){
 
-  handler::object_pos moveit;
-  moveit.move.data = true;
-  std::cout << "x-position: " << std::endl;
-  std::cin >> moveit.location.x;
-  std::cout << "y-position: " << std::endl;
-  std::cin >> moveit.location.y;
-  std::cout << "z-position: " << std::endl;
-  std::cin >> moveit.location.z;
-  while (true||ros::ok()){
-  move_pub.publish(moveit);
   }
 
-
-  spin();
+  spinOnce();
+  loop_rate.sleep();
 
   return 0;
 
