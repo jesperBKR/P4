b6c8f7a8b2f36c9b2cc5d4bd849b3b751e13dbd4
raggyftw <hansenoliie@gmail.com>
2018-05-03 12:49:51 +0200

    new controller

--- handler/src/handler.cpp
+++ handler/src/handler.cpp
@@ -19,81 +19,78 @@ public:
   //Constructor
   Handler(){
     //Subscribers
-  //position_sub = nh.subscribe("RS_object", 1, &Handler::positionCallback,this);
+    position_sub = nh.subscribe("RS_object", 1, &Handler::positionCallback,this);
     gui_sub = nh.subscribe("GUI_feed", 1, &Handler::guiCallback,this);
     process_sub = nh.subscribe("JACO_feedback",1,&Handler::processCallback,this);
 
     //Publishers
     move_pub = nh.advertise<rupee_msgs::object_pos>("JACO_goal", 1);
     feedback_pub = nh.advertise<std_msgs::Int8>("GUI_feedback", 1);
-    //repetition_pub = nh.advertise<std_msgs::Int8>("Reps_status",1);  // Test to check if counter goes  up.
   }
   //Destructor
   ~Handler(){
 
   }
 
-/*
-    //Oliver test stuff //*****
-    moveit.move.data = true;
-    //}
-    //else{
-    //  moveit.move.data = false;
-    //}
-    moveit.difficulty.data = dif;
-    moveit.exercise.data = ex;
-    moveit.location.x = 0.1539178662002; //position_msg.location.x;
-    moveit.location.y = -0.661769986153; //position_msg.location.y;
-    moveit.location.z = 0.131685048342; //position_msg.location.z;
-    move_pub.publish(moveit);
-    //*****
-  }
-  //Object position, xyz... What should we do if it cannot find the object? Is it still publishing a point?
-  void positionCallback(const rupee_msgs::camera& position_msg){
-    ROS_INFO("Postion x:[%f], y: [%f], z: [%f]", position_msg.location.x,position_msg.location.y,position_msg.location.z);
-    //if(start && position_msg.detected.data){
-    moveit.move.data = true;
-    //}
-    //else{
-    //  moveit.move.data = false;
-    //}
-    moveit.difficulty.data = dif;
-    moveit.exercise.data = ex;
-    moveit.location.x = 0.1539178662002; //position_msg.location.x;
-    moveit.location.y = -0.661769986153; //position_msg.location.y;
-    moveit.location.z = 0.131685048342; //position_msg.location.z;
-    move_pub.publish(moveit);
-*/
-
-
-  //JACO number of repetitions completed
-  void processCallback(const std_msgs::Bool& process_msg){
-    ROS_INFO("Rep done: [%d]",process_msg.data);
-    if(start){
-      if(process_msg.data){   //Whenever JACO returns true the repetition counter goes up
-        reps++;
-      }
-    }
-
-  }
   //Input values from GUI: reps, diff, type, and if running (run)
   void guiCallback(const rupee_msgs::Setup& gui_msg){
     ROS_INFO("Reps: [%d], Difficulty: [%d], Exercise Type: [%d], Move: [%d]", gui_msg.reps,gui_msg.diff,gui_msg.type, gui_msg.run);
     if(gui_msg.reps == 0 || !gui_msg.run){ //If gui not paused (reps = 0) or started (run = false) then it should not start
       start = false;
       if(!gui_msg.run){
-        reps = 0;
+        rep = 0;
       }
     }
     else{
       start = true;
       dif = gui_msg.diff;
       ex = gui_msg.type;
-      reps = gui_msg.reps;
-      feedback.data = reps;
+      feedback.data = rep;
     }
     feedback_pub.publish(feedback); //Number of repetitions done
-}
+
+//    //Oliver test stuff //*****
+//    moveit.move.data = true;
+//    //}
+//    //else{
+//    //  moveit.move.data = false;
+//    //}
+//    moveit.difficulty.data = dif;
+//    moveit.exercise.data = ex;
+//    moveit.location.x = 0.1539178662002; //position_msg.location.x;
+//    moveit.location.y = -0.661769986153; //position_msg.location.y;
+//    moveit.location.z = 0.131685048342; //position_msg.location.z;
+//    move_pub.publish(moveit);
+//    //*****
+  }
+  //Object position, xyz... What should we do if it cannot find the object? Is it still publishing a point?
+  void positionCallback(const rupee_msgs::camera& position_msg){
+    ROS_INFO("Postion x:[%f], y: [%f], z: [%f]", position_msg.location.x,position_msg.location.y,position_msg.location.z);
+    if(start && position_msg.detected.data){
+    moveit.move.data = true;
+    }
+    else{
+      moveit.move.data = false;
+    }
+    moveit.difficulty.data = dif;
+    moveit.exercise.data = ex;
+    moveit.location.x = position_msg.location.x;
+    moveit.location.y = position_msg.location.y;
+    moveit.location.z = position_msg.location.z;
+    move_pub.publish(moveit);
+
+  }
+  //JACO number of repetitions completed
+  void processCallback(const std_msgs::Bool& process_msg){
+    ROS_INFO("Rep done: [%d]",process_msg.data);
+    if(start){
+      if(process_msg.data){   //Whenever JACO returns true the repetition counter goes up
+        rep++;
+      }
+    }
+
+  }
+
 private:
   //ROS
   NodeHandle nh;
@@ -102,7 +99,6 @@ private:
   Subscriber process_sub;
   Publisher move_pub;
   Publisher feedback_pub;
-  Publisher repetition_pub;
 
   //Objects/Messages
   rupee_msgs::object_pos moveit;
@@ -110,7 +106,7 @@ private:
 
   //Variables
   bool start;
-  int reps;
+  int rep;
   int dif;
   int ex;
 };
--- rupee_controller/scripts/rupee_controller.py
+++ rupee_controller/scripts/rupee_controller.py
@@ -1,42 +1,48 @@
 #! /usr/bin/env python
-"""A helper program to test cartesian goals for the JACO and MICO arms."""
-
+#ROS libraries
 import roslib; roslib.load_manifest('kinova_demo')
 import rospy
-
+import actionlib
+#Python libraries
 import sys
 import numpy as np
-
-import actionlib
+from random import uniform
+import math
+#ROS messages
 import kinova_msgs.msg
-from kinova_msgs.msg import ArmPoseAction
 import std_msgs.msg
+import geometry_msgs.msg
+from kinova_msgs.msg import ArmPoseAction
 from rupee_msgs.msg import object_pos
 from rupee_msgs.msg import Setup
 from std_msgs.msg import String
 from std_msgs.msg import Int8
 from std_msgs.msg import Bool
-import geometry_msgs.msg
-from random import uniform
-import math
-import argparse
+from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import PoseStamped
 
-""" Global variable """
-arm_joint_number = 0
-finger_number = 0
+
+""" Global variables """
+#arm_joint_number = 0
+#finger_number = 0
+#finger_maxDist = 18.9/2/1000  # max distance for one finger
+#currentCartesianCommand = [0.212322831154, -0.257197618484, 0.509646713734, 1.63771402836, 1.11316478252, 0.134094119072] # default home in unit mq
+#ori2 =[0.962,  0.267,  -0.000,  0.067]
+#done = True
 prefix = 'j2n6s300_'
-finger_maxDist = 18.9/2/1000  # max distance for one finger
 finger_maxTurn = 6800  # max thread rotation for one finger
-currentCartesianCommand = [0.212322831154, -0.257197618484, 0.509646713734, 1.63771402836, 1.11316478252, 0.134094119072] # default home in unit mq
 home_pos  = [0.212322592735,-0.256329715252,0.5066832304]
 home_ori  = [0.64524179697,0.319341748953,0.42331302166,0.549990773201]
 home_pose = [home_pos,home_ori]
-object_location = home_pos
+workspace_constant = [0.0,-0.60,0.1]
+object_location = [0,0,0]
+offset = 0.10
 move = False
-done = False
+loop = True
 exercise_type = 1
 exercise_difficulty = 1
-orientation = [0.962,  0.267,  -0.000,  0.067]
+orientation = [0.962,  0.267,  -0.000,  0.067]#[0.999,0.01,-0.02,0.005]
+temp_location = [0,0,0]
 
 
 
@@ -52,9 +58,6 @@ def cartesian_pose_client(position, orientation):
         x=position[0], y=position[1], z=position[2])
     goal.pose.pose.orientation = geometry_msgs.msg.Quaternion(
         x=orientation[0], y=orientation[1], z=orientation[2], w=orientation[3])
-
-    # print('goal.pose in client 1: {}'.format(goal.pose.pose)) # debug
-
     client.send_goal(goal)
 
     if client.wait_for_result(rospy.Duration(10.0)):
@@ -71,11 +74,9 @@ def gripper_client(finger_positions):
     client = actionlib.SimpleActionClient(action_address,
                                           kinova_msgs.msg.SetFingersPositionAction)
     client.wait_for_server()
-
     goal = kinova_msgs.msg.SetFingersPositionGoal()
     goal.fingers.finger1 = float(finger_positions[0])
     goal.fingers.finger2 = float(finger_positions[1])
-    # The MICO arm has only two fingers, but the same action definition is used
     if len(finger_positions) < 3:
         goal.fingers.finger3 = 0.0
     else:
@@ -87,84 +88,139 @@ def gripper_client(finger_positions):
         client.cancel_all_goals()
         rospy.WARN('the gripper action timed-out')
         return None
+
+def fingerAction(action):
+    if(action == "close"):
+	    finger_val = [75,75,75]
+    elif(action == "open"):
+	    finger_val = [0,0,0]
+    finger_turn_command = [x/100.0 * finger_maxTurn for x in finger_val]
+    finger_turn_ = finger_turn_command
+    try:
+        positions_temp1 = [max(0.0, n) for n in finger_turn_]
+        positions_temp2 = [min(n, finger_maxTurn) for n in positions_temp1]
+        positions = [float(n) for n in positions_temp2]
+        result = gripper_client(positions)
+    except rospy.ROSInterruptException:
+            print('program interrupted before completion')
+
 def moveArm(gripper_pos,ori,finger_status):
     success = cartesian_pose_client(gripper_pos,ori)
     while not success:
-        print("Moving to goal")
+        pass
     if not finger_status == "":
         fingerAction(finger_status)
-'''
-def kinova_robotTypeParser(kinova_robotType_):
-    """ Argument kinova_robotType """
-    global robot_category, robot_category_version, wrist_type, arm_joint_number, robot_mode, finger_number, prefix, finger_maxDist, finger_maxTurn
-    robot_category = kinova_robotType_[0]
-    robot_category_version = int(kinova_robotType_[1])
-    wrist_type = kinova_robotType_[2]
-    arm_joint_number = int(kinova_robotType_[3])
-    robot_mode = kinova_robotType_[4]
-    finger_number = int(kinova_robotType_[5])
-    prefix = kinova_robotType_ + "_"
-    finger_maxDist = 18.9/2/1000  # max distance for one finger in meter
-    finger_maxTurn = 6800  # max thread turn for one finger
-'''
+    #JACOFeedbackPub(False)
 
-def random_coordinates(workspace_limit):
-    random_x = uniform(workspace_limit[0],workspace_limit[1])
-    random_y = uniform(workspace_limit[2],workspace_limit[3])
-    random_z = uniform(workspace_limit[4],workspace_limit[5])
+def exerciseRun(exercise,difficulty):
+    print (exercise)
+    if exercise == 1:
+        exerciseOne(difficulty)
+    elif exercise == 2:
+        exerciseTwo(difficulty)
+    elif exercise == 3:
+        exerciseThree(difficulty)
+    elif exercise == 4:
+        exerciseFour(difficulty)
+    elif exercise == 5:
+        exerciseFive(difficulty)
+    else:
+        print("Exercise type must be a number between 1-5.")
+
+def exerciseOne(difficulty):
+    rand_pos = randomCoordinatesOne(difficulty)
+    moveArm(object_location,orientation,"close")
+    moveArm(rand_pos,orientation,"open")
+    print('Random position is :', rand_pos )
+    moveArm(home_pose[0],home_pose[1],"")
+def exerciseTwo(difficulty):
+    rand_pos = randomCoordinatesTwo(difficulty)
+    moveArm(object_location,orientation,"close")
+    #moveArm(rand_pos,orientation,"")
+    #print('Random position is :', rand_pos )
+    #moveArm(home_pose[0],home_pose[1],"")
+def exerciseThree(difficulty):
+    pass
+def exerciseFour(difficulty):
+    pass
+def exerciseFive(difficulty):
+    pass
+
+def randomCoordinatesOne(difficulty):
+    workspace =[difficulty*0.14,difficulty*0.11]
+    if (difficulty == 3):
+        workspace[1] = 2*0.11
+    random_x = workspace_constant[0] + uniform(-workspace[0],workspace[0])
+    random_y = workspace_constant[1] + uniform(0.0,workspace[1])
+    random_z = workspace_constant[2]
+    random_pos  = [random_x,random_y,random_z]
+    if difficulty > 1:
+        sub_workspace = [workspace_constant[0]+(difficulty-1)*0.14,workspace_constant[1]+(difficulty-1)*0.11]
+        if (object_location[0]-0.05 <= random_x <= object_location[0] + 0.05 and object_location[1]-0.05 <= random_y <= object_location[1] + 0.05
+        or  -sub_workspace[0] <= random_x <= sub_workspace[0] and workspace_constant[1] <= random_y <= sub_workspace[1]):
+            return (randomCoordinatesOne(difficulty))
+    else:
+        if(object_location[0]-0.05 <= random_x <= object_location[0] + 0.05 and object_location[1]-0.05 <= random_y <= object_location[1] + 0.05):
+            return (randomCoordinatesOne(difficulty))
+    return random_pos
+
+def randomCoordinatesTwo(difficulty):
+    global temp_location
+    workspace =[difficulty*0.14,difficulty*0.11,difficulty*0.11]
+    if (difficulty == 3):
+        workspace[1] = 2*0.11
+    random_x = workspace_constant[0] + uniform(-workspace[0],workspace[0])
+    random_y = workspace_constant[1] + uniform(0.0,workspace[1])
+    random_z = workspace_constant[2] + uniform(0.0,workspace[2])
+    random_pos  = [random_x,random_y,random_z]
+    if difficulty > 1:
+        sub_workspace = [workspace_constant[0]+(difficulty-1)*0.14,workspace_constant[1]+(difficulty-1)*0.11]
+        if (temp_location[0]-0.05 <= random_x <= temp_location[0] + 0.05 and temp_location[1]-0.05 <= random_y <= temp_location[1] + 0.05 and temp_location[2]-0.05 <= random_z <= temp_location[2] + 0.05
+        or  -sub_workspace[0] <= random_x <= sub_workspace[0] and workspace_constant[1] <= random_y <= sub_workspace[1]):
+            return (randomCoordinatesTwo(difficulty))
+    else:
+        if (temp_location[0]-0.05 <= random_x <= temp_location[0] + 0.05 and temp_location[1]-0.05 <= random_y <= temp_location[1] + 0.05 and temp_location[2]-0.05 <= random_z <= temp_location[2] + 0.05):
+            return (randomCoordinatesTwo(difficulty))
+    temp_location = random_pos
+    return random_pos
+
+def randomCoordinatesThree(difficulty):
+    random_x = workspace_constant[0] + uniform(0,1)
+    random_y = workspace_constant[1] + uniform(0,1)
+    random_z = 0
     random_pos  = [random_x,random_y,random_z]
     return random_pos
 
-def determineWorkspace(difficulty):
-    if difficulty == 1:
-        workspace = [0.14, 0.787,-0.34,-0.74,0.05,0.7]
-        #workspace = [x,-x,y,-y,z,-z] #TODO set z to be tabel height
-    elif difficulty == 2:
-        workspace = [x,-x,y,-y,z,-z]
-    elif difficulty == 3:
-        workspace = [x,-x,y,-y,z,-z]
-    elif difficulty == 4:
-        workspace = [x,-x,y,-y,z,-z]
-    elif difficulty == 5:
-        workspace = [x,-x,y,-y,z,-z]
-    return workspace
+def randomCoordinatesFour(difficulty):
+    random_x = workspace_constant[0] + uniform(0,1)
+    random_y = workspace_constant[1] + uniform(0,1)
+    random_z = 0
+    random_pos  = [random_x,random_y,random_z]
+    return random_pos
 
-def setWorkspace(difficulty,exercise):
-    if exercise == 1:
-        workspace = determineWorkspace(difficulty)
-    elif exercise == 2:
-        workspace = determineWorkspace(difficulty)
-    elif exercise == 3:
-        workspace = determineWorkspace(difficulty)
-    elif exercise == 4:
-        workspace = determineWorkspace(difficulty)
-    elif exercise == 5:
-        workspace = determineWorkspace(difficulty)
-    return workspace
+def randomCoordinatesFive(difficulty):
+    random_x = workspace_constant[0] + uniform(0,1)
+    random_y = workspace_constant[1] + uniform(0,1)
+    random_z = 0
+    random_pos  = [random_x,random_y,random_z]
+    return random_pos
 
+
+'''
 def unitParser( finger_value_):
     finger_turn_command = [x/100.0 * finger_maxTurn for x in finger_value_]
     finger_turn_ = finger_turn_command
     return finger_turn_
+'''
 
-def fingerAction(action):
-    try:
-        positions_temp1 = [max(0.0, n) for n in unitParser(gripperAction(action))]
-        positions_temp2 = [min(n, finger_maxTurn) for n in positions_temp1]
-        positions = [float(n) for n in positions_temp2]
-        print('Sending finger position ...')
-        result = gripper_client(positions)
-        print('Finger position sent!')
-    except rospy.ROSInterruptException:
-        print('program interrupted before completion')
-
+'''
 def gripperAction(status):
 	if status == "close":
 	    finger_val = [75,75,75]
 	elif status == "open":
 	    finger_val = [0,0,0]
 	return finger_val
-
+'''
 
 def handlerCallback(handler_data):
     global move
@@ -175,63 +231,35 @@ def handlerCallback(handler_data):
     exercise_type       = handler_data.exercise.data
     object_location[0]  = handler_data.location.x
     object_location[1]  = handler_data.location.y
-    object_location[2]  = handler_data.location.z
+    object_location[2]  = handler_data.location.z + offset
     move                = handler_data.move.data
+def fingerCallback(Wrench):
+    print("Hej")
+    #torque_total  = wrench.torque.x + wrench.torque.y + wrench.torque.z
+    print (Wrench.wrench.torque.x)
 
-def JACOfeedback():
+def JACOFeedbackPub(done):
     pub = rospy.Publisher('JACO_feedback', Bool, queue_size=10)
     rate = rospy.Rate(10) # 10hz
     rep_done = done
     pub.publish(rep_done)
     rate.sleep()
 
-def exerciseRun(exercise,difficulty):
-    if (exercise == 1 or 2 or 3 or 4):
-        if exercise == 1:
-            moveArm(object_location,orientation,"close")
-            workspace = setWorkspace(exercise,difficulty)
-            rand_pos = random_coordinates(workspace)
-            moveArm(rand_pos,orientation,"open")
-            #moveArm(object_location,orientation,"close")
-            moveArm(home_pose[0],home_pose[1],"open")
-        elif exercise == 2:
-            moveArm(object_location,orientation,"close")
-            workspace = setWorkspace(exercise,difficulty)
-            rand_pos = random_coordinates(workspace)
-            moveArm(rand_pos,orientation,"open")
-            moveArm(home_pose[0],home_pose[1],"open")
-        elif exercise == 3:
-            print("TODO Exercise 3")
-            #retreiveObject(object_location)
-            #TODO implement skeletal tracking to implement.
-            #setWorkspace(exercise,difficulty)
-        elif exercise == 4:
-            moveArm(object_location,orientation,"close")
-            workspace = setWorkspace(exercise,difficulty)
-            rand_pos_1 = random_coordinates(workspace)
-            rand_pos_2 = random_coordinates(workspace)
-            moveArm(rand_pos_1,"")
-            moveArm(rand_pos_2,"")
-            #TODO stop when patient's hand is close to the EE so he can take object.
-    elif exercise == 5:
-        print("Todo chase and touch")
-        #TODO chase and touch, add later.
-    else:
-        print("Exercise type must be a number between 1-5.")
 
 if __name__ == '__main__':
     rospy.init_node(prefix + 'pose_action_client')
-    #kinova_robotTypeParser(prefix)
     moveArm(home_pose[0],home_pose[1],"open")
     while not rospy.is_shutdown():
         rospy.Subscriber("JACO_goal", object_pos, handlerCallback)
+        rospy.Subscriber("/j2n6s300_driver/out/tool_wrench",WrenchStamped, fingerCallback)
+        if(loop):
+            JACOFeedbackPub(True)
+            loop = False
         if(move):
             try:
-                done = False
                 exerciseRun(exercise_type,exercise_difficulty)
-                done = True
-                JACOfeedback()
                 print('Rep done')
+                loop = True
             except rospy.ROSInterruptException:
                 print "program interrupted before completion"
         else:
